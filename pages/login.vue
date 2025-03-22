4d39c16bb7b9ec0f43c47e9e88a05e2ba7e92337
Ririri1997 <ircher1997@gmail.com>
2025-03-22 13:08:39 +0300

    fix ts

--- pages/login.vue
+++ pages/login.vue
@@ -8,30 +8,44 @@
  </div>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
-const username = ref('');
-const password = ref('');
-const errorMessage = ref('');
+interface UserCredentials {
+ username: string;
+ passphrase: string;
+}
+interface User {
+ credentials: UserCredentials;
+ active: boolean;
+}
+
+const username = ref<string>('');
+const password = ref<string>('');
+const errorMessage = ref<string>('');
 const router = useRouter();
 
 const login = async () => {
  // Загружаем JSON с пользователями. Вообще не безопасное хранение 
- const { data: users } = await useFetch('/users.json');
+ const { data: users } = await useFetch<User[]>('/users.json');
 
+ // захотелось добавить чуть-чуть валидации 
+ if (!username.value || !password.value) {
+  errorMessage.value = 'Введите данные';
+  return
+ }
  // Ищем пользователя
- const user = users.value.find(u =>
+ const user: User | null = users.value?.find((u: User) =>
   u.credentials.username === username.value &&
   u.credentials.passphrase === password.value
- );
+ ) || null;
 
  if (user && user.active) {
   localStorage.setItem('user', JSON.stringify(user));
   router.push('/account');
  } else {
-  errorMessage.value = 'Неверные данные или аккаунт не активен';
+  errorMessage.value = 'Введены неверные данные авторизации. Попробуйте ещё раз';
  }
 };
 </script>
